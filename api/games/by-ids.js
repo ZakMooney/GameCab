50422ca436683eaf725f45d12ceaf20c5ba30916
Zak <zmooney@hotmail.co.uk>
2025-07-14 21:41:28 +0100

    feat: vercel cors handling

--- api/games/by-ids.js
+++ api/games/by-ids.js
@@ -2,6 +2,11 @@
 import { IGDBService } from '../_lib/igdb-service.js';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Max-Age', '86400');
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
